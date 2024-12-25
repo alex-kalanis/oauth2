1deb140726a89829700e01a634a91146a390338f
Petr Plsek <me@kalanys.com>
2024-12-25 13:06:54 +0100

    Scrutinizer services 2

--- php-tests/TestCase.php
+++ php-tests/TestCase.php
@@ -25,10 +25,10 @@ abstract class TestCase extends Tester\TestCase
 
         $config = $configurator->setTempDirectory(TEMP_DIR);
         $config->addStaticParameters([
-            'OAUTH2_MYSQL_DB_HOST' => strval(getenv('OAUTH2_MYSQL_DB_HOST')),
-            'OAUTH2_MYSQL_DB_NAME' => strval(getenv('OAUTH2_MYSQL_DB_NAME')),
-            'OAUTH2_MYSQL_DB_USER' => strval(getenv('OAUTH2_MYSQL_DB_USER')),
-            'OAUTH2_MYSQL_DB_PASS' => strval(getenv('OAUTH2_MYSQL_DB_PASS')),
+            'OAUTH2_MYSQL_DB_HOST' => strval(getenv('OAUTH2_MYSQL_DB_HOST')) ?: '127.0.0.1',
+            'OAUTH2_MYSQL_DB_NAME' => strval(getenv('OAUTH2_MYSQL_DB_NAME')) ?: 'testing',
+            'OAUTH2_MYSQL_DB_USER' => strval(getenv('OAUTH2_MYSQL_DB_USER')) ?: 'testing',
+            'OAUTH2_MYSQL_DB_PASS' => strval(getenv('OAUTH2_MYSQL_DB_PASS')) ?: null,
         ]);
         $config->addConfig(__DIR__ . DIRECTORY_SEPARATOR . 'config.neon');
 
